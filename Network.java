91886b13cef16b366a8287475126644837078197
Iddo Kenigsberg <Iddo.Kenigsberg@post.runi.ac.il>
2026-01-11 15:33:08 +0200

    16

--- Network.java
+++ Network.java
@@ -159,8 +159,8 @@ public class Network {
 
     // Returns a textual description of all the users in this network, and who they follow.
     public String toString() {
-        System.out.println("Network:");
-       String ans ="";
+       
+       String ans ="Network:";
        for(int i=0;i<userCount;i++)
        {
             ans+=  System.lineSeparator();
